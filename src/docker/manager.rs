a78bd4dbf7ec4b030ecdb8a1bd99ab694f2638a8
Matt Miller <mattmillerf@pm.me>
2023-11-28 23:20:23 -0500

    refactor list_instances

--- src/docker/manager.rs
+++ src/docker/manager.rs
@@ -1,6 +1,7 @@
 use shiplift::{Docker, NetworkCreateOptions};
 use shiplift::builder::ContainerOptions;
 use shiplift::builder::ContainerListOptions;
+use shiplift::rep::Container;
 use log::{info, error};
 use std::collections::HashMap;
 
@@ -66,6 +67,34 @@ pub async fn create_instance(
     }
 }
 
+async fn list_instances(
+    docker: &Docker,
+    network_name: &str,
+    containers: Vec<Container>
+) -> Result<HashMap<String, crate::Instance>, shiplift::Error> {
+    let mut instances: HashMap<String, crate::Instance> = HashMap::new();
+
+    for container in containers {
+        let details = docker.containers().get(&container.id).inspect().await?;
+        let network_settings = &details.network_settings;
+
+        if let Some(labels) = &details.config.labels {
+            if network_settings.networks.contains_key(network_name) {
+                if let Some(instance_label) = labels.get("instance") {
+                    instances.entry(instance_label.to_string())
+                        .or_insert_with(|| crate::Instance {
+                            container_ids: Vec::new(),
+                            uuid: instance_label.to_string()
+                        })
+                        .container_ids.push(container.id);
+                }
+            }
+        }
+    }
+
+    Ok(instances)
+}
+
 pub async fn list_all_instances(docker: &Docker, network_name: &str) -> Result<HashMap<String, crate::Instance>, shiplift::Error> {
     let containers = docker
         .containers()
@@ -73,27 +102,7 @@ pub async fn list_all_instances(docker: &Docker, network_name: &str) -> Result<H
               .all()
               .build())
         .await?;
-    let mut instances: HashMap<String, crate::Instance> = HashMap::new();
-
-    for container in containers {
-        let details = docker.containers().get(&container.id).inspect().await?;
-        let network_settings = &details.network_settings;
-
-        if let Some(labels) = &details.config.labels {
-            if network_settings.networks.contains_key(network_name) {
-                if let Some(instance_label) = labels.get("instance") {
-                    instances.entry(instance_label.to_string())
-                        .or_insert_with(|| crate::Instance {
-                            container_ids: Vec::new(),
-                            uuid: instance_label.to_string()
-                        })
-                        .container_ids.push(container.id);
-                }
-            }
-        }
-    }
-
-    Ok(instances)
+    list_instances(docker, network_name, containers).await
 }
 
 pub async fn list_running_instances(docker: &Docker, network_name: &str) -> Result<HashMap<String, crate::Instance>, shiplift::Error> {
@@ -101,27 +110,7 @@ pub async fn list_running_instances(docker: &Docker, network_name: &str) -> Resu
         .containers()
         .list(&ContainerListOptions::default())
         .await?;
-    let mut instances: HashMap<String, crate::Instance> = HashMap::new();
-
-    for container in containers {
-        let details = docker.containers().get(&container.id).inspect().await?;
-        let network_settings = &details.network_settings;
-
-        if let Some(labels) = &details.config.labels {
-            if network_settings.networks.contains_key(network_name) {
-                if let Some(instance_label) = labels.get("instance") {
-                    instances.entry(instance_label.to_string())
-                        .or_insert_with(|| crate::Instance {
-                            container_ids: Vec::new(),
-                            uuid: instance_label.to_string()
-                        })
-                        .container_ids.push(container.id);
-                }
-            }
-        }
-    }
-
-    Ok(instances)
+   list_instances(docker, network_name, containers).await
 }
 
 pub async fn start_all_containers_in_instance(docker: &Docker, network_name: &str, instance_uuid: &str) -> Result<(), shiplift::Error> {
@@ -129,8 +118,16 @@ pub async fn start_all_containers_in_instance(docker: &Docker, network_name: &st
 
     if let Some(instance) = instances.get(instance_uuid) {
         for container_id in &instance.container_ids {
-            // Start each container in the specified instance
-            docker.containers().get(container_id).start().await?;
+            match docker.containers().get(container_id).start().await {
+                Ok(container) => {
+                    info!("{} container successfully started: {:?}", container_id, container);
+                    Ok(())
+                }
+                Err(err) => {
+                    error!("Error starting {} container: {:?}", container_id, err);
+                    Err(err)
+                }
+            }?;
         }
     }
 
@@ -138,12 +135,20 @@ pub async fn start_all_containers_in_instance(docker: &Docker, network_name: &st
 }
 
 pub async fn stop_all_containers_in_instance(docker: &Docker, network_name: &str, instance_uuid: &str) -> Result<(), shiplift::Error> {
-    let instances = list_all_instances(docker, network_name).await?;
+    let instances = list_running_instances(docker, network_name).await?;
 
     if let Some(instance) = instances.get(instance_uuid) {
         for container_id in &instance.container_ids {
-            // Start each container in the specified instance
-            docker.containers().get(container_id).stop(None).await?;
+            match docker.containers().get(container_id).stop(None).await {
+                Ok(container) => {
+                    info!("{} container successfully stopped: {:?}", container_id, container);
+                    Ok(())
+                }
+                Err(err) => {
+                    error!("Error stopping {} container: {:?}", container_id, err);
+                    Err(err)
+                }
+            }?;
         }
     }
 
@@ -155,8 +160,16 @@ pub async fn delete_all_containers_in_instance(docker: &Docker, network_name: &s
 
     if let Some(instance) = instances.get(instance_uuid) {
         for container_id in &instance.container_ids {
-            // Start each container in the specified instance
-            docker.containers().get(container_id).delete().await?;
+            match docker.containers().get(container_id).delete().await {
+                Ok(container) => {
+                    info!("{} container successfully deleted: {:?}", container_id, container);
+                    Ok(())
+                }
+                Err(err) => {
+                    error!("Error deleting {} container: {:?}", container_id, err);
+                    Err(err)
+                }
+            }?;
         }
     }
 
@@ -168,8 +181,16 @@ pub async fn restart_all_containers_in_instance(docker: &Docker, network_name: &
 
     if let Some(instance) = instances.get(instance_uuid) {
         for container_id in &instance.container_ids {
-            // Start each container in the specified instance
-            docker.containers().get(container_id).restart(None).await?;
+            match docker.containers().get(container_id).restart(None).await {
+                Ok(container) => {
+                    info!("{} container successfully restarted: {:?}", container_id, container);
+                    Ok(())
+                }
+                Err(err) => {
+                    error!("Error restarting {} container: {:?}", container_id, err);
+                    Err(err)
+                }
+            }?;
         }
     }
 
@@ -180,9 +201,7 @@ pub async fn stop_all_instances(docker: &Docker, network_name: &str) -> Result<(
     let running_instances = list_running_instances(docker, network_name).await?;
 
     for (_, instance) in running_instances.iter() {
-        for container_id in &instance.container_ids {
-            docker.containers().get(container_id).stop(None).await?;
-        }
+        stop_all_containers_in_instance(docker, network_name, &instance.uuid).await?;
     }
 
     Ok(())
