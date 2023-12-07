58301167956544ae830cc2647d9db11f2b4dfa68
Matt Miller <mattmillerf@pm.me>
2023-12-07 09:08:14 -0500

    remove wp-cli image

--- wpdev_core/src/lib.rs
+++ wpdev_core/src/lib.rs
@@ -8,7 +8,6 @@ pub mod docker;
 
 pub const NETWORK_NAME: &str = "wp-network";
 pub const WORDPRESS_IMAGE: &str = "wordpress:latest";
-pub const WORDPRESS_CLI_IMAGE: &str = "wordpress:cli";
 pub const NGINX_IMAGE: &str = "nginx:latest";
 pub const MYSQL_IMAGE: &str = "mysql:latest";
 pub const ADMINER_IMAGE: &str = "adminer:latest";
